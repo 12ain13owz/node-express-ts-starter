53e200f2e71824d2750eb861b8ee796fe90bebe2
Phaiboon Withanthamrong <touchfn@gmail.com>
2026-08-05 12:04:51 +0700

    update app name for easy replace

--- src/shared/constants/app.const.ts
+++ src/shared/constants/app.const.ts
@@ -1,4 +1,4 @@
-export const APP_NAME = 'Node Starter'
+export const APP_NAME = 'node-starter'
 
 export const APP_GENERIC = {
   serverListening: (baseUrl: string) => `Server listening at ${baseUrl}`,
